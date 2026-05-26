501db32247ffd884107835b6b89627523c086a58
antonyjenishfernando <antonyjenishfernando.27it@licet.ac.in>
2026-05-26 10:43:30 +0530

    Fix: Use Home instead of HomeMinimal in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import HomeMinimal from "./pages/HomeMinimal";
+import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
 import Analytics from "./components/Analytics";
 
@@ -17,7 +17,7 @@ const App = () => (
       <BrowserRouter>
         <Analytics />
         <Routes>
-          <Route path="/" element={<HomeMinimal />} />
+          <Route path="/" element={<Home />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
